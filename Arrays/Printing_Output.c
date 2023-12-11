d53f73237093f4e9ef03f00a5b8c012637a83127
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-11 18:04:19 +0530

    Create Printing_Output.c
    Printing Output Using Loop

---
+++ Arrays/Printing_Output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {4,5,3,2,5};
+    
+    //Printing Using Loop
+    for(int i=0; i<5; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
