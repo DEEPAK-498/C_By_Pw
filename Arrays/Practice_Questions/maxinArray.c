a6eaab093ca61f127dae5bd41af909451af12cd1
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-12 23:04:41 +0530

    maxinArray

---
+++ Arrays/Practice_Questions/maxinArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <limits.h> //For Usig Min,Max Function
+
+int main(){
+    int arr[] = {1,4,5,3,6};
+    int n = sizeof(arr)/4;
+
+    // int mx = arr[0];
+    int mx = INT_MIN; /*This Also Works Fine. Basically INT_MIN Defines The Smallest Number.*/
+
+    for(int i=0; i<n; i++){
+        if(mx<arr[i]) mx = arr[i];
+    }
+    printf("%d\n", mx);
+    return 0;
+}
