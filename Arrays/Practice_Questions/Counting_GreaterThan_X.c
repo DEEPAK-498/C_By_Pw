cd02812fd229d49e752945070061a80b96a05723
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-13 20:26:36 +0530

    Create Counting_GreaterThan_X.c
    Counting and Printing the number of elements in given array grater than the given number X.

---
+++ Arrays/Practice_Questions/Counting_GreaterThan_X.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int size = sizeof(arr)/4;
+
+    for(int i=0; i<size; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    int x = 5;
+    int count = 0;
+    
+    //Counting
+    for(int i=0; i<size; i++){
+        if(arr[i] > x){
+            count++; //Count how many numbers are >x
+            printf("%d ", arr[i]); //Print the numbers That are >x
+        }
+    }
+    printf("\n");
+    printf("%d", count); //Print how many numbers >x
+    printf("\n");
+    return 0;
+}
