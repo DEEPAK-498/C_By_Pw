a3dee9f54f4a7728fed7ced59f7df078487e6e53
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-13 18:55:32 +0530

    Update minInArray.c

--- Arrays/Practice_Questions/minInArray.c
+++ Arrays/Practice_Questions/minInArray.c
@@ -13,7 +13,7 @@ int main(){
     //Value Of INT_MAX
     printf("%d\n", mn_1);
 
-    for(int i=0; i<n; i++){
+    for(int i=0; i<n; i++){     //Here "i" Initialise with 0
         if(mn_1>arr[i]) mn_1 = arr[i];
     }
     printf("Methode 1 Output : %d\n", mn_1);
@@ -21,7 +21,7 @@ int main(){
     //METHODE 2
     int mn_2 = arr[0];
     
-    for(int i=1; i<n; i++){
+    for(int i=1; i<n; i++){     //Here "i" Initialise with 1
         if(mn_2>arr[i]) mn_2 = arr[i];
     }
     printf("Methode 2 Output : %d\n", mn_2);
