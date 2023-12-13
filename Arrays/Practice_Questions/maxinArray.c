4cd8b4b398f55215b10d9854850e5e2ca726febe
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-13 18:54:26 +0530

    Update maxinArray.c

--- Arrays/Practice_Questions/maxinArray.c
+++ Arrays/Practice_Questions/maxinArray.c
@@ -5,15 +5,25 @@ int main(){
     int arr[] = {1,4,5,3,6};
     int n = sizeof(arr)/4;
 
-    // int mx = arr[0];  /* We Can Use The First Element Also For This We Have to Start The Loop From The 1St index.*/
-    int mx = INT_MIN; /*This Also Works Fine. Basically INT_MIN Defines The Smallest Number.*/
+    // int mx = arr[0]; /* We Can Use The First Element Also For This We Have to Start The Loop From The 1St index.*/
 
-    //Value Of INT_MIN
-    printf("%d\n", mx);
+    //METHODE 1
+    int mx_1 = INT_MIN; /*This Also Works Fine. Basically INT_MIN Defines The Smallest Number.*/
     
-    for(int i=0; i<n; i++){
-        if(mx<arr[i]) mx = arr[i];
+    //Value Of INT_MIN
+    printf("%d\n", mx_1);
+
+    for(int i=0; i<n; i++){    //Here "i" Initialise with 0
+        if(mx_1<arr[i]) mx_1 = arr[i];
     }
-    printf("%d\n", mx);
+    printf("Methode 1 Output : %d\n", mx_1);
+
+    //METHODE 2
+    int mx_2 = arr[0];
+    
+    for(int i=1; i<n; i++){     //Here "i" Initialise with 1
+        if(mx_2<arr[i]) mx_2 = arr[i];
+    }
+    printf("Methode 2 Output : %d\n", mx_2);
     return 0;
 }
