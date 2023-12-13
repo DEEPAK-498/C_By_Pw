5bdf875f1b40911e5b85138d6f60ffb83c0ac0bd
Deepak Kumar Das <72337342+DEEPAK-498@users.noreply.github.com>
2023-12-13 18:42:56 +0530

    Update minInArray.c

--- Arrays/Practice_Questions/minInArray.c
+++ Arrays/Practice_Questions/minInArray.c
@@ -5,15 +5,15 @@ int main(){
     int arr[] = {1,4,5,3,6};
     int n = sizeof(arr)/4;
 
-    // int mx = arr[0]; /*We Can Start With The First Element Also For That We HAve To Start The loop From The 1st Index*/
-    int mx = INT_MAX; /*This Also Works Fine. Basically INT_MIN Defines The Smallest Number.*/
+    // int mn = arr[0]; /*We Can Start With The First Element Also*/
+    int mn = INT_MAX; /*This Also Works Fine. Basically INT_MIN Defines The Smallest Number.*/
     
     //Value Of INT_MAX
-    printf("%d\n", mx);
+    printf("%d\n", mn);
 
     for(int i=0; i<n; i++){
-        if(mx>arr[i]) mx = arr[i];
+        if(mn>arr[i]) mn = arr[i];
     }
-    printf("%d\n", mx);
+    printf("%d\n", mn);
     return 0;
 }
